89d3a3a4a7b65d2efdfbd6432601c2ccf0573c07
Rahul Sheth <Rahul@Rahuls-MacBook-Pro-3.local>
2020-03-28 15:37:58 -0700

    Test for User login

--- backend/gamify/views.py
+++ backend/gamify/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import json
 # Create your views here.
+
+@api_view(['POST'])
+def login(request, format=None):
+	email = request.POST['email']
+	response = HttpResponse()
+	if !User.get(email=email):
+		# Need to create a user
+		User.create(email=email, name=request.POST['name'])
+		response.status_code = 201
+		return response
+	else:
+		user_serialized = json.dumps(User.get(email=email), default=str)
+		response.status_code = 200
+		response.content = user_serialized
+		return response
