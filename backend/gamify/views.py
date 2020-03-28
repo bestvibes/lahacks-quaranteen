d4e8faf949b00a37a32d79a8faea58479adffb91
Rahul Sheth <Rahul@Rahuls-MacBook-Pro-3.local>
2020-03-28 16:55:34 -0700

    Changed serializer

--- backend/gamify/views.py
+++ backend/gamify/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import json
+from django.core import serializers
 # Create your views here.
 
 @api_view(['POST'])
@@ -10,12 +10,12 @@ def login(request, format=None):
 	if not User.get(email=email):
 		# Need to create a user
 		u =	User.create(email=email, name=request.POST['name'])
-		user_serialized = json.dumps(u, default=str)
+		user_serialized = serializers.serialize("json", u)
 		response.content = user_serialized
 		response.status_code = 201
 		return response
 	else:
-		user_serialized = json.dumps(User.get(email=email), default=str)
+		user_serialized = serializers.serialize("json", User.get(email=email))
 		response.status_code = 200
 		response.content = user_serialized
 		return response
