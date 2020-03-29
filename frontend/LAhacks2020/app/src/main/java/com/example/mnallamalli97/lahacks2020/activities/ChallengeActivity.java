0639b348cf803fc9372d9239b2b65a632994bee8
cpedada <cpedada@ucla.edu>
2020-03-29 10:37:36 -0700

    challenges screen

--- frontend/LAhacks2020/app/src/main/java/com/example/mnallamalli97/lahacks2020/activities/ChallengeActivity.java
+++ frontend/LAhacks2020/app/src/main/java/com/example/mnallamalli97/lahacks2020/activities/ChallengeActivity.java
@@ -3,6 +3,7 @@ package com.example.mnallamalli97.lahacks2020.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.mnallamalli97.lahacks2020.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -49,5 +50,12 @@ public class ChallengeActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.getMenu().findItem(R.id.navigation_challenges).setChecked(true);
+
+        TextView t1 = findViewById(R.id.textView3);
+        TextView t2 = findViewById(R.id.textView5);
+        TextView t3 = findViewById(R.id.textView6);
+        t1.setText("Iron Chef");
+        t2.setText("Quarantine and Chill");
+        t3.setText("Quaranthings To Do");
     }
 }
