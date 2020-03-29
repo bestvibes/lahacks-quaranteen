1bd92f8ff7539cfebfa5dc91cca7e678c4f9fabb
aniveera1 <22436531+aniveera1@users.noreply.github.com>
2020-03-28 17:15:51 -0700

    Finished up team views

--- backend/gamify/views.py
+++ backend/gamify/views.py
@@ -1,36 +1,67 @@
-from rest_framework import viewsets
+from django.core import serializers
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.core import serializers
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.viewsets import ModelViewSet
+
 from gamify.models import Team, User
 from gamify.serializers import UserSerializer
-from rest_framework.decorators import api_view
+
 
 @api_view(['POST'])
 def createTeam(request):
     """
     Create a team
-    inputs: JSON with one field "name"
+    inputs: JSON with two field "name", "user_id"
     """
     data = JSONParser().parse(request)
     new_team = Team.objects.create(name=data['name'])
+    new_team.save()
+
+    user = User.objects.get(id=data["user_id"])
+    user.team = new_team
+    user.team_leader = True
+    user.save()
+
     return JsonResponse(serializers.serialize("json", new_team), status=201)
  
+
 @api_view(['POST'])
 def leaveTeam(request):
+    """
+    Leave a team
+    inputs: JSON with one field "user_id"
+    """
     data = JSONParser().parse(request)
-    User.objects.get(data["user_id"])
+    user = User.objects.get(id=data["user_id"])
+    # TODO: Hacky, easier to deal with data consistency by not letting team leaders leave
+    if user.team_leader:
+        return JsonResponse(serializers.serialize("json", user), status=403)
+    user.team = None
+    user.team_leader = False
+    user.save()
+    return JsonResponse(serializers.serialize("json", user), status=201)
+
 
 @api_view(['POST'])
 def joinTeam(request):
+    """
+    Join a team
+    inputs: JSON with three field "user_id", "join_code"
+    """
     data = JSONParser().parse(request)
     team_to_join = Team.objects.get(join_code=data["join_code"])
+    user = User.objects.get(id=data["user_id"])
+    user.team = team_to_join
+    user.team_leader = False
+    user.save()
+    return JsonResponse(serializers.serialize("json", user), status=201)
 
-class UserViewSet(viewsets.ModelViewSet):
+
+class UserViewSet(ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
--- backend/lahacks/urls.py
+++ backend/lahacks/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from gamify import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('models/', include(router.urls)),
+    path('', include('gamify.urls')),
 ]
