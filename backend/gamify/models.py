3d4611469fc9b2335b1d3cc455a61d42619393f3
Anirudh Veeraragavan <aniveera1@gmail.com>
2020-03-28 19:01:07 -0700

    Fixed some bugs (#5)

--- backend/gamify/models.py
+++ backend/gamify/models.py
@@ -1,18 +1,9 @@
-import hashlib
-import time
-
 from django.db import models
 
 
 class Team(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     join_code = models.CharField(max_length=100, blank=False, null=False)
-
-    # Factory method b/c messing with constructor might break other things
-    def create(cls, name):
-        token = name + str(time.time())
-        # src: https://stackoverflow.com/questions/16008670/how-to-hash-a-string-into-8-digits
-        return Team(name=name, join_code=hashlib.sha256(token.encode('utf-8')).hexdigest()[:5])
 
 
 class User(models.Model):
--- backend/gamify/views.py
+++ backend/gamify/views.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from django.core import serializers
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
@@ -16,7 +19,10 @@ def createTeam(request):
     inputs: JSON with two field "name", "user_id"
     """
     data = JSONParser().parse(request)
-    new_team = Team.objects.create(name=data['name'])
+    new_team = Team(
+        name=data['name'],
+        join_code=''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    )
     new_team.save()
 
     user = User.objects.get(id=data["user_id"])
@@ -24,7 +30,8 @@ def createTeam(request):
     user.team_leader = True
     user.save()
 
-    return JsonResponse(serializers.serialize("json", new_team), status=201)
+    data = serializers.serialize("json", Team.objects.filter(id=new_team.id))
+    return JsonResponse(data, status=201, safe=False)
  
 
 @api_view(['POST'])
@@ -37,11 +44,17 @@ def leaveTeam(request):
     user = User.objects.get(id=data["user_id"])
     # TODO: Hacky, easier to deal with data consistency by not letting team leaders leave
     if user.team_leader:
-        return JsonResponse(serializers.serialize("json", user), status=403)
+        return JsonResponse(
+            serializers.serialize("json", User.objects.filter(id=user.id)),
+            status=403, safe=False
+        )
     user.team = None
     user.team_leader = False
     user.save()
-    return JsonResponse(serializers.serialize("json", user), status=201)
+    return JsonResponse(
+        serializers.serialize("json", User.objects.filter(id=user.id)),
+        status=201, safe=False
+    )
 
 
 @api_view(['POST'])
@@ -56,7 +69,10 @@ def joinTeam(request):
     user.team = team_to_join
     user.team_leader = False
     user.save()
-    return JsonResponse(serializers.serialize("json", user), status=201)
+    return JsonResponse(
+        serializers.serialize("json", User.objects.filter(id=user.id)),
+        status=201, safe=False
+    )
 
 
 class UserViewSet(ModelViewSet):
