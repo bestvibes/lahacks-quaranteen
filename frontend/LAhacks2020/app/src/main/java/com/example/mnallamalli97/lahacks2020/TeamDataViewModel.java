77ddae48e5386a158f8cd3da8e80ebe7c7e74869
cpedada <cpedada@ucla.edu>
2020-03-28 23:24:56 -0700

    complete team data viewModel and update UserDataViewModel

--- frontend/LAhacks2020/app/src/main/java/com/example/mnallamalli97/lahacks2020/TeamDataViewModel.java
+++ frontend/LAhacks2020/app/src/main/java/com/example/mnallamalli97/lahacks2020/TeamDataViewModel.java
@@ -1,5 +1,67 @@
 package com.example.mnallamalli97.lahacks2020;
 
-public class TeamDataViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
+import com.example.mnallamalli97.lahacks2020.utilities.NetworkUtils;
+import com.example.mnallamalli97.lahacks2020.utilities.TeamService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class TeamDataViewModel extends ViewModel {
+    private MutableLiveData<Team> team;
+
+    public TeamDataViewModel(){
+        team = new MutableLiveData<Team>();
+    }
+
+    public void createTeam(String name, int user_id){
+        TeamService service = NetworkUtils.getRetrofitInstance().create(TeamService.class);
+        Call<Team> call = service.createTeam(name, user_id);
+        call.enqueue(new Callback<Team>() {
+            @Override
+            public void onResponse(Call<Team> call, Response<Team> response) {
+                team.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<Team> call, Throwable t) {
+                team.setValue(null);
+            }
+        });
+    }
+
+    public void leaveTeam(int user_id){
+        TeamService service = NetworkUtils.getRetrofitInstance().create(TeamService.class);
+        Call<Team> call = service.leaveTeam(user_id);
+        call.enqueue(new Callback<Team>() {
+            @Override
+            public void onResponse(Call<Team> call, Response<Team> response) {
+                team.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<Team> call, Throwable t) {
+                team.setValue(null);
+            }
+        });
+    }
+
+    public void joinTeam(String name, String join_code){
+        TeamService service = NetworkUtils.getRetrofitInstance().create(TeamService.class);
+        Call<Team> call = service.joinTeam(name, join_code);
+        call.enqueue(new Callback<Team>() {
+            @Override
+            public void onResponse(Call<Team> call, Response<Team> response) {
+                team.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<Team> call, Throwable t) {
+                team.setValue(null);
+            }
+        });
+    }
 }
--- frontend/LAhacks2020/app/src/main/java/com/example/mnallamalli97/lahacks2020/UserDataViewModel.java
+++ frontend/LAhacks2020/app/src/main/java/com/example/mnallamalli97/lahacks2020/UserDataViewModel.java
@@ -20,6 +20,7 @@ public class UserDataViewModel extends ViewModel {
 
     public UserDataViewModel() {
         users = new MutableLiveData<List<User>>();
+        updatedUser = new MutableLiveData<User>();
     }
 
     public void loadListUsers() {
@@ -40,7 +41,27 @@ public class UserDataViewModel extends ViewModel {
         }
     }
 
-    public LiveData<List<User>> getLiveData(){
+    public void updateUser(int user_id, String name) {
+        UserService service = NetworkUtils.getRetrofitInstance().create(UserService.class);
+        Call<User> call = service.updateUserName(user_id, name);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                updatedUser.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                users.setValue(null);
+            }
+        });
+    }
+
+    public LiveData<List<User>> getUsersLiveData(){
         return users;
+    }
+
+    public LiveData<User> getUpdatedUserLiveData() {
+        return updatedUser;
     }
 }
