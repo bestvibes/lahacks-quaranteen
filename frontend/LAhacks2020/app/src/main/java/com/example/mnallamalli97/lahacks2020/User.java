98c98ca8944f9a3e2505bf4d9f85e60c079583e9
cpedada <cpedada@ucla.edu>
2020-03-28 22:40:30 -0700

    update TeamService to reflect API and create API Response, update User object to contain user_id

--- frontend/LAhacks2020/app/src/main/java/com/example/mnallamalli97/lahacks2020/User.java
+++ frontend/LAhacks2020/app/src/main/java/com/example/mnallamalli97/lahacks2020/User.java
@@ -1,26 +1,35 @@
 package com.example.mnallamalli97.lahacks2020;
 
+import androidx.annotation.Nullable;
+
 import com.google.gson.annotations.SerializedName;
 
 public class User {
+    @SerializedName("id")
+    private int user_id;
     @SerializedName("name")
     private String name;
     @SerializedName("team")
-    private String team;
+    @Nullable private Integer team;
     @SerializedName("team_leader")
     private Boolean team_leader;
 
-    public User(String name, String team, Boolean team_leader){
+    public User(int user_id, String name, @Nullable Integer team, Boolean team_leader){
+        this.user_id = user_id;
         this.name = name;
         this.team = team;
         this.team_leader = team_leader;
+    }
+
+    public int getUser_id() {
+        return user_id;
     }
 
     public String getName(){
         return name;
     }
 
-    public String getTeam(){
+    public Integer getTeam(){
         return team;
     }
 
