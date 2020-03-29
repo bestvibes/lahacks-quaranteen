829eddb43c6d13908a7e58d5d0be754d8bcc710b
Rahul Sheth <56454700+rahulssheth@users.noreply.github.com>
2020-03-28 23:34:08 -0700

    Challenge views (#9)
    * fix half-working challenge views
    
    * Fixed some small bugs/database issues
    
    * challenge views
    
    Co-authored-by: Vaibhav Aggarwal <vaibhavagg98@gmail.com>
    Co-authored-by: aniveera1 <22436531+aniveera1@users.noreply.github.com>

--- backend/gamify/models.py
+++ backend/gamify/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 
@@ -9,22 +11,8 @@ class Team(models.Model):
 class User(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     email = models.CharField(max_length=100, blank=False, null=False)
-    team = models.ForeignKey(Team, on_delete=models.SET_NULL, blank=True, null=True)  # many-to-one
+    team = models.ForeignKey(Team, related_name="members", on_delete=models.SET_NULL, blank=True, null=True)  # many-to-one
     team_leader = models.BooleanField(blank=False, null=False, default=False)
-
-
-class MasterTask(models.Model):
-    name = models.CharField(max_length=100, blank=False, null=False)
-    description = models.CharField(max_length=512, blank=False, null=False)
-    verification_url = models.CharField(max_length=512, blank=False, null=False)
-    point_value = models.IntegerField(blank=False, null=False)
-
-
-class TaskInstance(models.Model):
-    task = models.ForeignKey(MasterTask, on_delete=models.CASCADE, blank=False, null=False)  # many-to-one
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)  # many-to-one
-    date = models.DateField(auto_now_add=True)
-    completed = models.BooleanField(blank=False, null=False)
 
 
 class TeamScore(models.Model):
@@ -36,14 +24,36 @@ class MasterChallenge(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     description = models.CharField(max_length=512, blank=False, null=False)
     prize = models.CharField(max_length=100, blank=False, null=False)
-    mastertasks = models.ManyToManyField(MasterTask)  # many-to-many
     daily_point_limit = models.IntegerField(blank=False, null=False)
     team_size_limit = models.IntegerField(blank=False, null=False)
     number_of_days = models.IntegerField(blank=False, null=False)
 
 
 class ChallengeInstance(models.Model):
-    challenge = models.ForeignKey(MasterChallenge, on_delete=models.CASCADE, blank=False, null=False)  # many-to-one
-    teams = models.ManyToManyField(Team)  # many-to-many
-    tasks = models.ManyToManyField(TaskInstance)  # many-to-many
-    scores = models.ManyToManyField(TeamScore)  # many-to-many
+    challenge = models.ForeignKey(MasterChallenge, related_name="challenge_instances", on_delete=models.CASCADE, blank=False, null=False)  # many-to-one
+    teams = models.ManyToManyField(Team, blank=True)  # many-to-many
+    scores = models.ManyToManyField(TeamScore, blank=True)  # many-to-many
+    start_time = models.DateTimeField(blank=False, null=False)
+
+    def is_in_progress(self):
+        time_now = datetime.datetime.now(self.start_time.tzinfo)
+        return (time_now > self.start_time and time_now < self.start_time + datetime.timedelta(days=self.challenge.number_of_days))
+
+    def has_ended(self):
+        time_now = datetime.datetime.now(self.start_time.tzinfo)
+        return (time_now >= self.start_time + datetime.timedelta(days=self.challenge.number_of_days))
+
+
+class MasterTask(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    challenge = models.ForeignKey(MasterChallenge, related_name="master_tasks", on_delete=models.CASCADE, blank=False, null=False) # many-to-one
+    description = models.CharField(max_length=512, blank=False, null=False)
+    verification_url = models.CharField(max_length=512, blank=False, null=False)
+    point_value = models.IntegerField(blank=False, null=False)
+
+class TaskInstance(models.Model):
+    challenge = models.ForeignKey(ChallengeInstance, related_name="tasks", on_delete=models.CASCADE, blank=False, null=False)
+    task = models.ForeignKey(MasterTask, related_name="instances", on_delete=models.CASCADE, blank=False, null=False)  # many-to-one
+    user = models.ForeignKey(User, related_name="tasks", on_delete=models.CASCADE, blank=False, null=False)  # many-to-one
+    date = models.DateField()
+    completed = models.BooleanField(blank=False, null=False)
--- backend/gamify/urls.py
+++ backend/gamify/urls.py
@@ -2,18 +2,25 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from gamify.views import createTeam, joinTeam, leaveTeam, login, UserViewSet
+from gamify.views import createTeam, joinChallenge, joinTeam, leaveTeam, login, getUserTasks, ChallengeInstanceViewSet, UserViewSet, MasterChallengeViewSet, MasterTaskViewSet, TaskInstanceViewSet, TeamViewSet
 from gamify.verification.views import verifyApple, verifyVideo, verifyBook, verifySunrise
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
+router.register('teams', TeamViewSet)
+router.register('challenges', ChallengeInstanceViewSet)
+router.register('masterchallenges', MasterChallengeViewSet)
+router.register('mastertasks', MasterTaskViewSet)
+router.register('tasks', TaskInstanceViewSet)
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('models/', include(router.urls)),
+    path('tasks/byuser/', getUserTasks, name='get-user-tasks'),
     path('team/create/', createTeam, name='team-create'),
     path('team/leave/', leaveTeam, name='team-leave'),
     path('team/join/', joinTeam, name='team-join'),
+    path('challenge/join/', joinChallenge, name='challenge-join'),
     path('verify/apple/', verifyApple, name='verify-apple'),
     path('verify/video/', verifyVideo, name='verify-video'),
     path('verify/book/', verifyBook, name='verify-book'),
