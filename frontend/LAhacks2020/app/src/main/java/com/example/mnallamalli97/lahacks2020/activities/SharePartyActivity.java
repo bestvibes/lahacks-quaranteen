a11cd016dc85eb1c402498dd32ff2cc4ee17b68f
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2020-03-29 04:45:11 -0700

    frontend: share team screen

--- frontend/LAhacks2020/app/src/main/java/com/example/mnallamalli97/lahacks2020/activities/SharePartyActivity.java
+++ frontend/LAhacks2020/app/src/main/java/com/example/mnallamalli97/lahacks2020/activities/SharePartyActivity.java
@@ -1,9 +1,13 @@
 package com.example.mnallamalli97.lahacks2020.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mnallamalli97.lahacks2020.R;
+import com.example.mnallamalli97.lahacks2020.TeamDataViewModel;
 
 public class SharePartyActivity extends AppCompatActivity {
 
@@ -12,6 +16,12 @@ public class SharePartyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.share_party);
+
+        TextView code = findViewById(R.id.uniqueCodeTextView);
+        code.setText(TeamDataViewModel.getTeamData().getValue().getJoinCode());
+
+        // until we can implement this
+        findViewById(R.id.shareButton).setVisibility(View.GONE);
     }
 
 }
